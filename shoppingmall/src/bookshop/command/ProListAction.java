f7ac31f66be141660a1fb466a1052a80b29a1b53
Bora <dksemqh97@naver.com>
2017-04-27 15:19:07 +0900

    promain/shopmain/mngr DBBean 페이지 수정 -보라

--- shoppingmall/src/bookshop/command/ProListAction.java
+++ shoppingmall/src/bookshop/command/ProListAction.java
@@ -9,19 +9,20 @@ import bookshop.bean.MngrDBBean;
 import bookshop.bean.MngrDataBean;
 
 public class ProListAction implements CommandAction {
+	//승환 873page~
 	@Override
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
 		List<MngrDataBean> bookList = null;
 		int count = 0;
 		String book_kind = request.getParameter("book_kind");
 
-		MngrDBBean bookProcess = MngrDBBean.getInstance();
+		MngrDBBean bookProcess = MngrDBBean.get_instance();
 
 		// kind 값이 all이면 전체 상품의 수를 얻어냄
 		if (book_kind.equals("all"))
-			count = bookProcess.getBookCount();
+			count = bookProcess.getBook_count();
 		else // all이 아니면 해당 카테고리의 상품 수를 얻어냄
-			count = bookProcess.getBookCount(book_kind);
+			count = bookProcess.getBook_count(book_kind);
 
 		if (count > 0) { // 상품이 있으면 수행
 			// 상품 목록을 얻어냄
